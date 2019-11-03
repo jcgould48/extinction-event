3fa8490a81bf724bc42129c8eb1bbcd17532314b
Jesse Gould <jesse.gould@codeimmersives.com>
2019-11-03 15:55:26 -0500

    complete

--- main.js
+++ main.js
@@ -1,14 +1,46 @@
-document.querySelector('div.app ol').addEventListener('click', lineThrough)
-document.querySelector('div.app ul').addEventListener('click', disappear)
-//document.querySelector('div.app ul').addEventListener('click', lineThrough)
+const firstOlLi = document.querySelectorAll('ol li')
+const firstUlLi = document.querySelectorAll('ul li')
+const picRow = document.querySelectorAll('#row img')
+const button = document.querySelector('#toggle')
+
+for (let i = 0; i < firstOlLi.length; i++) {
+    firstOlLi[i].addEventListener('click', olFunc);
+}
+function olFunc(element){
+    element.target.style.textDecoration = 'line-through';
+}
 
 
-function lineThrough(event) {
-event.target.style.textDecoration = 'line-through'
+for (let i = 0; i < firstUlLi.length; i++) {
+    firstUlLi[i].addEventListener('click', ulFunc);
 }
-function disappear(event) {
-    event.target.style.opacity = 0
+function ulFunc(element){
+    element.target.style.opacity = 0;
 }
-// function lineThrough(event) {
-//     event.target.style.textDecoration = 'line-through'
-// }
+
+for (let i = 0; i < picRow.length; i++) {
+    picRow[i].addEventListener('click', imgFunc);
+}
+function imgFunc(element){
+    element.target.style.width = '0px';
+}
+
+
+button.addEventListener('click', extinction)
+
+function extinction(){
+    for(let i = 0; i < firstOlLi.length; i++) {
+        firstOlLi[i].style.textDecoration = 'line-through';
+    }
+
+    for(let i = 0; i < firstUlLi.length; i++) {
+        firstUlLi[i].style.opacity = 0;
+    }
+
+    for(let i = 0; i < picRow.length; i++){
+        picRow[i].style.width = '0px';
+    }
+}
+
+
+
